a14824efcb29ceaf636f4ee28eaf06f3b91408f9
Norelax <wownorelax@mail.ru>
2023-02-27 19:16:15 +0300

    zadacha1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int a = new Random().Next(0, 1000); 
+int b = new Random().Next(0, 1000); 
+Console.Write("a=");
+Console.Write(a);
+if (a > b)
+{
+    Console.Write(" больше ");
+}
+else if (a == b)
+{
+    Console.Write(" равен ");
+}
+else
+{
+    Console.Write(" меньше ");
+};
+Console.Write("b=");
+Console.WriteLine(b);
